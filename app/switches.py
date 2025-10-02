33a5db9e5fbf2e3d4b8666e99277fa693a30f64e
Luke Opperman <luke@opperman.net>
2025-10-02 10:21:15 -0400

    GPIO reading for www-data

--- app/switches.py
+++ app/switches.py
@@ -1,18 +1,15 @@
 try:
-    import RPi.GPIO as GPIO
+    import gpiod
 except ImportError:
-    GPIO = None
+    gpiod = None
 
 off_pin = 16
 debug_pin = 12
 
-    
-def mode():
-    if not GPIO:
-        return 'NOT_PI_DEVICE'
 
-    GPIO.setmode(GPIO.BCM)
-    # Define GPIO pin for checking
+def mode():
+    if not gpiod:
+        return 'NOT_PI_DEVICE'
     mode = "ACTIVE"  # possible modes are OFF or DEBUG or ACTIVE
     if pin_connected_to_ground(debug_pin):
         mode = "DEBUG"
@@ -20,14 +17,17 @@ def mode():
         mode = "OFF"
         # We won't hit this as the device will shutdown
     return mode
-    
+
 # Function to check for connection to ground
 def pin_connected_to_ground(pin):
-    # Set an internal pull-up resistor (optional, some circuits might have one already)
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    # Read the pin value
-    pin_value = GPIO.input(pin)
-
-    # If pin value is LOW (0), then it's connected to ground
-    return pin_value == 0
+    with gpiod.request_lines(
+            "/dev/gpiochip4",
+            consumer="mode",
+            config={
+                pin: gpiod.LineSettings(
+                    direction=gpiod.line.Direction.INPUT,
+                    active_low=True
+                )
+            },
+    ) as request:
+        return request.get_value(pin)
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y apache2 libapache2-mod-wsgi-py3 python3-virtualenv
-sudo cat > /etc/apache2/sites-available/001-mothbox.conf <<EOF
+sudo bash -c 'cat > /etc/apache2/sites-available/001-mothbox.conf <<EOF
 <VirtualHost *:80>
 	ServerName mothbox.local
 
@@ -25,9 +25,10 @@ sudo cat > /etc/apache2/sites-available/001-mothbox.conf <<EOF
 	</Directory>
 </VirtualHost>
 
-EOF
+EOF'
+
 chmod +x /home/pi
-sudo a2ensite mothbox
+sudo a2ensite 001-mothbox
 sudo a2enmod ssl
 
 cd /home/pi/Desktop/Mothbox/Web
@@ -35,10 +36,7 @@ virtualenv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
 
-openssl req -x509 -out localssl.pem -keyout localssl.key \                             	 
-  -newkey rsa:2048 -nodes -sha256 \
-  -subj '/CN=mothbox.local' -extensions EXT -config <( \
-   printf "[dn]\nCN=mothbox.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:mothbox.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+openssl req -x509 -out localssl.pem -keyout localssl.key   -newkey rsa:2048 -nodes -sha256  -subj '/CN=mothbox.local' -extensions EXT -config <( printf "[dn]\nCN=mothbox.local\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:mothbox.local\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 sudo mv localssl.pem /etc/ssl/certs/
 sudo mv localssl.key /etc/ssl/private/
@@ -52,3 +50,5 @@ sudo echo 'pi,www-data ALL=(ALL) NOPASSWD:/home/pi/Desktop/Mothbox/scripts/MothP
 chmod +w /home/pi/Desktop/Mothbox/controls.txt
 chmod +w /home/pi/Desktop/Mothbox/schedule_settings.csv
 chmod +w /home/pi/Desktop/Mothbox/camera_settings.csv
+# And read GPIO
+sudo usermod -a -G gpio www-data
