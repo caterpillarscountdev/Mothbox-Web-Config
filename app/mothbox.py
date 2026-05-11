877944662336a984ac13609c6f72d9b1b3321e7e
Luke Opperman <luke@opperman.net>
2026-05-11 14:33:01 -0400

    slower

--- app/mothbox.py
+++ app/mothbox.py
@@ -135,7 +135,7 @@ def test_device(device):
                 return "No photos"
         case "camera_raw":
             testing.camera_raw()
-            return f"Camera is taking a hardware photo. <div hx-post='{url_for('test_device', device='photo_raw')}' hx-trigger='every 5s'></div>"
+            return f"Camera is taking a hardware photo. <div hx-post='{url_for('test_device', device='photo_raw')}' hx-trigger='every 15s'></div>"
         case "photo_raw":
             photo = testing.camera_raw_latest_photo()
             if photo:
